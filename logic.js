2acbd352bf48be6d3cd265f9ad629755dafe5f38
Gonzalez <0566184@students.dadeschools.net>
2018-05-04 14:03:24 -0400

    Done

--- logic.js
+++ logic.js
@@ -32,28 +32,48 @@ numGuesses = 9; chosenWord = fruitsList[Math.floor(Math.random() * fruitsList.le
     document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 }
 
+// where all comparisons are done
 function checkLetters(letter) {
+// boolean that will change if letter is in word
 var letterInWord = false;
-
+// check if letter exists in word
 for (var i = 0; i < numBlanks; i++) { if (chosenWord[i] === letter) { letterInWord = true;
  } } if (letterInWord) { for (var j = 0; j < numBlanks; j++) { if (chosenWord[j] === letter) { blanksAndSuccesses[j] = letter; } } console.log(blanksAndSuccesses);
   } else { wrongGuesses.push(letter); numGuesses--; }
 }
 
+// code that runs after each guess
 function roundComplete() {
+
+	// says how many wins, losses and guesses left
 console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter +
 " | NumGuesses: " + numGuesses);
 
+// -HTML UPDATES-
+
+// updates the html to reflect guesses
 document.getElementById("guesses-left").innerHTML = numGuesses; document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" "); if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) { winCounter++;
   alert("You win!"); document.getElementById("win-counter").innerHTML = winCounter; startGame(); } else if (numGuesses === 0) { lossCounter++; alert("You lose");
    document.getElementById("loss-counter").innerHTML = lossCounter; startGame(); }
 }
 
+
+
+
+// MAIN PROCESS (CONTROLS WHAT IS RAN)
 startGame();
 
+// initiates function to capture key clicks
 document.onkeyup = function(event) {
+
+	// converts clicks to lower letters
 var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+
+// runs the code to check guesses
 checkLetters(letterGuessed);
+
+// code that ends each round
 roundComplete();
+
 };
